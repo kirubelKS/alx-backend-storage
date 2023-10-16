4ea12c996c4b582d07a05ce39993b5d5603324c7
kirubelks <kirubel.n.gebeyehu@gmail.com>
2023-10-16 20:36:33 +0300

    code correction on tasks 2,4,6,7 and 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,4 +3,4 @@
 
 def list_all(mongo_collection):
     '''lists all the collections in a collection.'''
-    return [doc for doc in mongp_collection.find()]
+    return [doc for doc in mongo_collection.find()]
